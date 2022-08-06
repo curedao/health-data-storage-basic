dc541af28631bf1c7163da7f6c227fe1de44c484
Vincenzo Domina <54762917+vincenzodomina@users.noreply.github.com>
2022-08-06 21:05:56 +0200

    add remove-all.sql
    remove all from the setup sql file

---
+++ remove-all.sql
@@ -0,0 +1,33 @@
+-- drop trigger if exists on_auth_user_created;
+
+drop policy if exists "Allow logged-in read access" on users;
+drop policy if exists "Allow individual insert access" on users;
+drop policy if exists "Allow individual update access" on users;
+drop policy if exists "Allow logged-in read access" on users;
+drop policy if exists "Allow individual insert access" on variables;
+drop policy if exists "Allow individual delete access" on variables;
+drop policy if exists "Allow authorized delete access" on variables;
+drop policy if exists "Allow individual insert access" on measurements;
+drop policy if exists "Allow individual delete access" on measurements;
+drop policy if exists "Allow authorized delete access" on measurements;
+drop policy if exists "Allow individual update access" on measurements;
+drop policy if exists "Allow individual read access" on public.user_roles;
+
+drop function if exists public.handle_new_user;
+drop function if exists public.authorize;
+
+drop table if exists public.user_roles;
+drop table if exists public.role_permissions;
+drop table if exists public.user_tier;
+drop table if exists public.user_gender;
+
+drop table if exists variables;
+drop table if exists measurements;
+
+drop type if exists public.user_tier;
+
+-- drop table if exists users;
+
+drop type if exists public.user_gender;
+drop type if exists public.app_role;
+drop type if exists public.app_permission;
