6d53716f78204b6b57247108c07014fba98274f8
Vincenzo Domina <54762917+vincenzodomina@users.noreply.github.com>
2022-09-01 19:31:58 +0200

    Delete schemas directory

--- schemas/app_permission.sql
+++
@@ -1,4 +0,0 @@
-create type app_permission as enum ('variables.delete', 'measurements.delete');
-
-alter type app_permission owner to supabase_admin;
-
--- schemas/role_permissions_id_seq.sql
+++
@@ -1,8 +0,0 @@
-grant select, update, usage on sequence role_permissions_id_seq to postgres;
-
-grant select, update, usage on sequence role_permissions_id_seq to anon;
-
-grant select, update, usage on sequence role_permissions_id_seq to authenticated;
-
-grant select, update, usage on sequence role_permissions_id_seq to service_role;
-
